3bccedf19b8797d1e3311274ae126ba76849279e
Huy Viet <nguyentranhuyviet04022004@gmail.com>
2025-07-15 10:20:31 +0700

    init admin dashboard

---
+++ Client/app/scripts/features/adminDashboard/adminDashboard.component.js
@@ -0,0 +1,5 @@
+angular.module('adminDashboard').component('adminDashboard', {
+    templateUrl: 'scripts/features/adminDashboard/adminDashboard.html',
+    controller: 'adminDashboardController',
+    controllerAs: 'adminDashboardCtrl'
+});
---
+++ Client/app/scripts/features/adminDashboard/adminDashboard.controller.js
@@ -0,0 +1,3 @@
+angular.module('adminDashboard').controller('adminDashboardController', function () {
+
+});
